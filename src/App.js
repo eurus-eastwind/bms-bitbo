a6a4eadbc7088504fce00443fc9457abf157c129
eurus-eastwind <86453147+eurus-eastwind@users.noreply.github.com>
2022-08-20 11:00:08 -1100

    for modifications

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Home from './pages/exterior/Home';
+function App() {
+  return (
+    <div className = "App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
